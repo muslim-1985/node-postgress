49e286db3d7b67aed2ddf81584c83ba598d17270
muslim <muslim.krum.85@gmail.com>
2019-10-06 16:09:25 +0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,81 @@
+const app = require('express');
+const exp = app();
+const route = app.Router();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+//const Artist = require('../controllers/artists');
+const Admin = require('../http/controllers/admin');
+const BotUsers = require('../http/controllers/botUsers');
+const LittleBot = require('../http/controllers/LittleBot');
+const Auth = require('../http/controllers/Auth');
+const multer = require('multer');
+const {checkAuth} = require('../http/middlewares/checkAuth');
+const {validateRegister, validateLogin, validateRoles} = require('../http/middlewares/validate');
+
+function rot() {
+    let routes = route.stack;
+    routes = routes.filter(route => typeof route.route != 'undefined');
+    return routes
+}
+module.exports.rot = rot;
+
+//промежуточная функция сохранения файла на сервере и в бд
+const storage = multer.diskStorage({
+    destination (req, file, cb) {
+        cb(null, './public/images');
+    },
+    filename (req, file ,cb) {
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+});
+
+const upload = multer({storage});
+//json parse body parser
+exp.use(bodyParser.json());
+//cors enabled by libs
+exp.use(cors());
+
+exp.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+route.use(bodyParser.json());
+//enabled CORS
+route.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+    res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
+    next();
+});
+
+route.options('/', cors());
+route.options('/botUsers', cors());
+route.options('/botUsers:chatId', cors());
+
+route.get('/', checkAuth, Admin.resPage);
+//route.post('/setGood', checkAuth, upload.single('image'), Admin.setGood);
+//botUsers controller
+route.get('/admin', checkAuth, BotUsers.getAllUsers);
+route.get('/botUsers', checkAuth, BotUsers.getAllUsers);
+route.get('/userMessages/:chatId', checkAuth, BotUsers.getUserMessages);
+route.get('/userAdminMessages/:chatId', checkAuth, Admin.getUserAdminMessages);
+route.post('/deleteMessage', checkAuth, LittleBot.deleteMessage);
+
+route.post('/login', validateLogin(), Auth.login);
+
+route.post('/register', validateRegister(), Auth.register);
+route.post('/createRole', checkAuth, validateRoles(), Auth.createRole);
+route.get('/getRoutes', Auth.getAllRoutes);
+
+// route.post('/logout', (req, res) => {
+//     res.clearCookie('token');
+//     res.status(200).send({message: "Logout success."});
+// });
+//
+// route.post('/artists', checkAuth, Artist.setAr);
+// route.get('/artists', Artist.getAll);
+// route.get('/artists/:id', Artist.showOne);
+// route.put('/artists/:id/update', Artist.actionUpdate);
+// route.delete('/artists/:id/delete', Artist.actionDelete);
+
+    //routes.post(`/${config.app.botToken}`, LittleBot.BotMsg);
+
+module.exports = route;
---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const exp = express();
+//http server app
+const app = require('http').Server(exp);
+//web socket server app
+const io = require('socket.io')(app);
+require('./socket/index')(io);
+//файл подключения к БД
+const db = require('./db.connect');
+//Routes file export
+const routes = require('./src/routes');
+//config file required
+const config = require('./config/config');
+const {initPassportAuth} = require('./http/middlewares/checkAuth');
+
+//static file path
+exp.use(express.static(config.app.staticPath));
+//  Connect all our routes to our application
+exp.use('/', routes);
+//инициализируем авторизацию из подключенного модуля
+initPassportAuth();
+
+db.connect(`mongodb://${config.db.host}:${config.db.port}/${config.db.name}`, (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log('database connected');
+
+    app.listen(config.app.port, () => {
+        console.log('app started muslim bey');
+    })
+});
